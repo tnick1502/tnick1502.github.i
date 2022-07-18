8156cc6f8b98b904d5e46e7137deb4602846190d
tnick1502 <59287059+tnick1502@users.noreply.github.com>
2022-07-18 14:11:48 +0300

    Добавлено меню с прокруткой

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+const navItems = document.querySelectorAll(".menu_items[data-goto]");
+if (navItems.length > 0) {
+  navItems.forEach((item) => {
+    item.addEventListener("click", onNavItemClick);
+  });
+
+  function onNavItemClick(event) {
+    const navItem = event.target;
+    if (navItem.dataset.goto && document.getElementById(navItem.dataset.goto)) {
+      const gotoBlock = document.getElementById(navItem.dataset.goto);
+      const gotoBlockValue =
+        gotoBlock.getBoundingClientRect().top + pageYOffset;
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth",
+      });
+      event.preventDefault();
+    }
+  }
+}
